701818dc1a5f1eff80bce20212e36c62612f8611
Marco Cedaro <cedmax@users.noreply.github.com>
2021-10-06 17:39:28 +0100

    added tennis and weather

--- apps/i2021/components/icons.tsx
+++ apps/i2021/components/icons.tsx
@@ -1,11 +1,15 @@
-import { ReactComponent as athletics } from '../public/icons/athletics.svg';
 import { ReactComponent as archery } from '../public/icons/archery.svg';
+import { ReactComponent as aswc } from '../public/icons/aswc.svg';
+import { ReactComponent as athletics } from '../public/icons/athletics.svg';
 import { ReactComponent as baseball } from '../public/icons/baseball.svg';
 import { ReactComponent as bocce } from '../public/icons/bocce.svg';
+import { ReactComponent as boxing } from '../public/icons/boxing.svg';
 import { ReactComponent as canoeing } from '../public/icons/canoeing.svg';
+import { ReactComponent as climbing } from '../public/icons/climbing.svg';
+import { ReactComponent as cricket } from '../public/icons/cricket.svg';
 import { ReactComponent as cycling } from '../public/icons/cycling.svg';
-import { ReactComponent as other } from '../public/icons/other.svg';
 import { ReactComponent as eaa } from '../public/icons/eaa.svg';
+import { ReactComponent as entertainment } from '../public/icons/entertainment.svg';
 import { ReactComponent as fencing } from '../public/icons/fencing.svg';
 import { ReactComponent as fishing } from '../public/icons/fishing.svg';
 import { ReactComponent as football } from '../public/icons/football.svg';
@@ -13,34 +17,35 @@ import { ReactComponent as gaming } from '../public/icons/gaming.svg';
 import { ReactComponent as location } from '../public/icons/location.svg';
 import { ReactComponent as martialarts } from '../public/icons/martialarts.svg';
 import { ReactComponent as motocross } from '../public/icons/motocross.svg';
-import { ReactComponent as entertainment } from '../public/icons/entertainment.svg';
 import { ReactComponent as olympics } from '../public/icons/olympics.svg';
+import { ReactComponent as other } from '../public/icons/other.svg';
 import { ReactComponent as polo } from '../public/icons/polo.svg';
 import { ReactComponent as sailing } from '../public/icons/sailing.svg';
+import { ReactComponent as skating } from '../public/icons/skating.svg';
 import { ReactComponent as skiing } from '../public/icons/skiing.svg';
 import { ReactComponent as swimming } from '../public/icons/swimming.svg';
 import { ReactComponent as tennis } from '../public/icons/tennis.svg';
 import { ReactComponent as video } from '../public/icons/video.svg';
 import { ReactComponent as volleyball } from '../public/icons/volleyball.svg';
-import { ReactComponent as climbing } from '../public/icons/climbing.svg';
-import { ReactComponent as skating } from '../public/icons/skating.svg';
-import { ReactComponent as boxing } from '../public/icons/boxing.svg';
-import { ReactComponent as cricket } from '../public/icons/cricket.svg';
-import { ReactComponent as aswc } from '../public/icons/aswc.svg';
+import { ReactComponent as weather } from '../public/icons/weather.svg';
 
 type iconTypes = {
   [key: string]: React.ComponentType;
 };
 
 const icons: iconTypes = {
-  aswc,
   archery,
+  aswc,
   athletics,
   baseball,
   bocce,
+  boxing,
   canoeing,
+  climbing,
+  cricket,
   cycling,
   eaa,
+  entertainment,
   fencing,
   fishing,
   football,
@@ -48,20 +53,17 @@ const icons: iconTypes = {
   location,
   martialarts,
   motocross,
-  entertainment,
   olympics,
+  other,
   polo,
   sailing,
+  skating,
   skiing,
   swimming,
-  boxing,
-  cricket,
   tennis,
   video,
   volleyball,
-  other,
-  skating,
-  climbing,
+  weather,
 };
 
 export default icons;
