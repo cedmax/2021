163a58bcab8b888fb17a5f43f53bdf20e7f34dc3
Marco Cedaro <cedmax@users.noreply.github.com>
2021-10-11 15:44:45 +0100

    diving and climbing

--- libs/get-image.js
+++ libs/get-image.js
@@ -2,6 +2,10 @@ const { prompt } = require('enquirer');
 const download = require('image-downloader');
 const sharp = require('sharp');
 const webp = require('webp-converter');
+const fs = require('fs');
+const sizeOf = require('image-size');
+
+const path = './apps/i2021';
 
 (async () => {
   const { slug, url } = await prompt([
@@ -17,10 +21,33 @@ const webp = require('webp-converter');
     },
   ]);
 
-  const filename = `./apps/i2021/public/o/${slug}.jpg`;
+  const filename = `${path}/public/o/${slug}.jpg`;
   const newFilename = filename.replace('/o/', '/images/');
 
   await download.image({ url, dest: filename });
   await sharp(filename).resize({ width: 800 }).toFile(newFilename);
   await webp.cwebp(newFilename, newFilename.replace('.jpg', '.webp'), '-q 75');
+
+  const data = require(`.${path}/data.json`);
+  if (data.find(({ img }) => img === slug)) {
+    const newData = data.reduce((acc, { img, ...itemData }) => {
+      const slug = typeof img === 'string' ? img : img.slug;
+      const { width, height } = sizeOf(`${path}/public/images/${slug}.webp`);
+      imgData = { slug, width, height };
+
+      return [
+        ...acc,
+        {
+          ...itemData,
+          img: imgData,
+        },
+      ];
+    }, []);
+
+    fs.writeFileSync(
+      `${path}/data.json`,
+      JSON.stringify(newData, null, 2),
+      'utf8'
+    );
+  }
 })();
