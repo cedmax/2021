d7f9b7e3494079ac108eab2cfa75656c1ef7b41c
Marco Cedaro <cedmax@users.noreply.github.com>
2021-11-10 12:49:18 +0000

    better handling of the sprites

---
+++ libs/svg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ "$1" == "" ]]; then (echo 'version missing' && exit 1); fi
+rm ./apps/i2021/public/sprite-*.svg || true
+npx svgo -f ./apps/i2021/public/icons
+npx svg-sprite-generator -d ./apps/i2021/public/icons/ -o ./apps/i2021/public/sprite-$1.svg
+sed -i '' "s+const currentVersion = \'v[0-9]\'+const currentVersion = '$@'+" ./apps/i2021/components/icon.tsx
